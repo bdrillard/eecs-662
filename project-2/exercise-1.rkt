6ca095f5c4007860c3b6cc543ee1bbacbd6d83cd
aleksanderesk <aleksanderesk@gmail.com>
2015-10-05 21:58:10 -0500

    finishing test cases

--- project-2/exercise-1.rkt
+++ project-2/exercise-1.rkt
@@ -16,16 +16,20 @@
               bound-value
               (lookup name rest-ds))))))
 
+(define op-list
+  (list (list 'add +)
+        (list 'sub -)
+        (list 'mul *)
+        (list 'div /)))
+
 (define-type CFAE
   (num (n number?))
   (id (name symbol?))
-  (add (lhs CFAE?) (rhs CFAE?))
-  (sub (lhs CFAE?) (rhs CFAE?))
-  (mul (lhs CFAE?) (rhs CFAE?))
-  (div (lhs CFAE?) (rhs CFAE?))
+  (op (proc symbol?))
+  (binop (proc op?) (lhs CFAE?) (rhs CFAE?))
   (if0 (check CFAE?) (then-body CFAE?) (else-body CFAE?))
-  (fun (id symbol?) (body CFAE?)) ; functions are single argument? 
-  (app (fun-expr CFAE?) (expr CFAE?))) ; only applying lambdas?, nested lambdas?, multivariable expressions are nested lambdas
+  (fun (id symbol?) (body CFAE?))
+  (app (fun-expr CFAE?) (expr CFAE?)))
 
 (define-type DefrdSub
   (mtSub)
@@ -35,10 +39,8 @@
   (lambda (e ds)
     (type-case CFAE e
       (num (n) (num n))
-      (add (l r) (reify-cfae + (interp-cfae l ds) (interp-cfae r ds)))
-      (sub (l r) (reify-cfae - (interp-cfae l ds) (interp-cfae r ds)))
-      (mul (l r) (reify-cfae * (interp-cfae l ds) (interp-cfae r ds)))
-      (div (l r) (reify-cfae / (interp-cfae l ds) (interp-cfae r ds)))
+      (op (p) (cadr (assoc p op-list)))
+      (binop (p l r) (reify-cfae (interp-cfae p ds) (interp-cfae r ds) (interp-cfae l ds)))
       (id (i) (lookup i ds))
       (fun (i e) (fun i e))
       (app (f e) (let ((fun-val (interp-cfae f ds)))
@@ -46,6 +48,27 @@
                        (error 'interp-cfae "number cannot be applied to a value")
                        (interp-cfae (fun-body fun-val)
                                     (aSub (fun-id fun-val) (interp-cfae e ds) ds)))))
-      (if0 (c t e) (if (= 0 (num-n (interp-cfae c ds)))
+      (if0 (c t e) (if (equal? (num 0) (interp-cfae c ds))
                        (interp-cfae t ds)
                        (interp-cfae e ds))))))
+
+#| TESTS |#
+(test (interp-cfae (num 1) (mtSub)) (num 1))
+(test (interp-cfae (binop (op 'add) (num 1) (num 1)) (mtSub)) (num 2))
+(test (interp-cfae (app (fun 'x (binop (op 'add) (id 'x) (num 1))) (num 1)) (mtSub))
+      (num 2))
+(test (interp-cfae (app (fun 'x (app (id 'x) (num 3))) 
+                        (fun 'y (binop (op 'add) (id 'y) (num 1)))) (mtSub)) 
+      (num 4))
+(test (interp-cfae (if0 (num 1) (num 1) (num 2)) (mtSub)) (num 2))
+(test (interp-cfae (if0 (app (fun 'x (binop (op 'sub) (id 'x) (num 3))) (num 3))
+                        (num 0)
+                        (num 1))
+                   (mtSub))
+      (num 0))
+(test/exn (interp-cfae (app (fun 'x (binop (op 'add) (id 'x) (num 1))) (id 'y)) (mtSub))
+          "undefined id")
+(test/exn (interp-cfae (app (num 1) (num 1)) (mtSub)) 
+          "number cannot be applied")
+(test/exn (interp-cfae (binop (op 'mul) (fun 'x (id 'x)) (num 0)) (mtSub))
+          "arithmetic operand cannot be a function")
